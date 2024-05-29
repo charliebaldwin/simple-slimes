9d3cc5938bff3f3625916970a5b5a0691ccfdb2f
charlie <charlie@charliebaldwin.com>
2024-05-29 12:11:27 -0400

    added Fabric template

---
+++ src/main/java/art/cbaldwin/SimpleSlimesDataGenerator.java
@@ -0,0 +1,11 @@
+package art.cbaldwin;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SimpleSlimesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
